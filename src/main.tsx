2b5a37cd13db1d3e2804946d6d78a08ef0d259db
off1805 <ouafofotsoh@gmail.com>
2026-03-27 18:53:45 +0100

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Routes, Route, Link } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import LoginPage from '@/modules/auth/pages/LoginPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+     <Routes>
+
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
   </StrictMode>,
 )
